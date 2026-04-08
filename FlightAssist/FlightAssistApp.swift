ebd70f1d42a569184f294cb8c9c42be5c0623f8f
jumana <jmanhtrfan@gmail.com>
2026-04-08 18:18:22 +0300

    Initial Commit

---
+++ FlightAssist/FlightAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAssistApp.swift
+//  FlightAssist
+//
+//  Created by Jumana on 20/10/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct FlightAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
